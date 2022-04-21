f2fddd8b08477e2afd33dd432f8f3933d6acd71c
Erhan Özgüç <eozguc@hotmail.com>
2022-04-21 15:48:17 +0300

    Add files via upload
    C# 101

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            //HackerRank Sitesindeki 30 Days Challenge Day 1 Ödevi
+            string inputString = Console.ReadLine();
+            Console.WriteLine("Hello, World.");
+            inputString = "Welcome to 30 Days of Code!";
+            Console.WriteLine(inputString);
+            Console.ReadKey();
+        }
+    }
+}
