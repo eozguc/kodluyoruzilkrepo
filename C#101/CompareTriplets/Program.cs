4fe714bbdad15b64e143ff3f1d30d9bea418ca5d
Erhan Özgüç <eozguc@hotmail.com>
2022-04-22 20:08:15 +0300

    CompareTriplets
    https://www.hackerrank.com/challenges/compare-the-triplets/problem

---
+++ C#101/CompareTriplets/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Linq;
+using System.IO;
+using System.Collections.Generic;
+
+namespace CompareTriplets
+{
+    class Program
+    {
+        public static List<int> compareTriplets(List<int> a, List<int> b)
+        {
+            int aliceScor=0;
+            int bobScor=0;
+            for(int i=0;i<a.Count;i++)
+            {
+                if(a[i]>b[i])
+                {
+                    aliceScor=aliceScor+1;
+                }
+                else if(a[i]<b[i])
+                {
+                    bobScor=bobScor+1;
+                }
+               
+            }
+            List<int> scor=new List<int>();
+            scor.Add(aliceScor);
+            scor.Add(bobScor);
+            return scor;
+        }
+
+        
+        static void Main(string[] args)
+        {
+            TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+            List<int> a = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(aTemp => Convert.ToInt32(aTemp)).ToList();
+
+            List<int> b = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(bTemp => Convert.ToInt32(bTemp)).ToList();
+
+            List<int> result = compareTriplets(a, b);
+
+            textWriter.WriteLine(String.Join(" ", result));
+
+            textWriter.Flush();
+            textWriter.Close();
+        }
+    }
+}
