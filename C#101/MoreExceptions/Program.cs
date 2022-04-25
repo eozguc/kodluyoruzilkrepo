2756ed35c5f5dbfe1958ba03467eaf68ad81759b
Erhan Özgüç <eozguc@hotmail.com>
2022-04-25 23:44:48 +0300

    MoreExceptions
    Day 17: More Exceptions

---
+++ C#101/MoreExceptions/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace MoreExceptions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Calculator myCalculator = new Calculator();
+            int T = int.Parse(Console.ReadLine());
+            while (T-- > 0)
+            {
+                string[] num = Console.ReadLine().Split();
+                int n = int.Parse(num[0]);
+                int p = int.Parse(num[1]);
+                try
+                {
+                    int ans = myCalculator.power(n, p);
+                    Console.WriteLine(ans);
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                }
+            }
+           
+        }
+       
+    }
+    class Calculator
+    {
+        public int power(int n, int p)
+        {
+            if (n < 0 || p < 0)
+            {
+                throw new Exception("n and p should be non-negative");
+            }
+            else
+            {
+                return Convert.ToInt32(Math.Pow(n, p));
+            }
+        }
+    }
+}
