b9c8826c2f9922fdddf99dc8a62058b33f7f3e24
Erhan Özgüç <eozguc@hotmail.com>
2022-04-26 01:33:57 +0300

    Generics
    Day 21: Generics

---
+++ C#101/Generics/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Generics
+{
+    internal class Program
+    {
+        static void PrintArray<Element>(Element[] array)
+        {
+            foreach (var element in array)
+            {
+                Console.WriteLine(element);
+            }
+        }
+
+        static void Main(String[] args)
+        {
+			int n = Convert.ToInt32(Console.ReadLine());
+			int[] intArray = new int[n];
+			for (int i = 0; i < n; i++)
+			{
+				intArray[i] = Convert.ToInt32(Console.ReadLine());
+			}
+
+			n = Convert.ToInt32(Console.ReadLine());
+			string[] stringArray = new string[n];
+			for (int i = 0; i < n; i++)
+			{
+				stringArray[i] = Console.ReadLine();
+			}
+
+			PrintArray<Int32>(intArray);
+			PrintArray<String>(stringArray);
+		}
+    }
+}
