3619552902d7cf68a8b2089762dff47389d6a8ed
Erhan Özgüç <eozguc@hotmail.com>
2022-04-25 23:13:26 +0300

    Add files via upload

---
+++ C#101/ExceptionsStringToInteger/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ExceptionsStringToInteger
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = Console.ReadLine();
+
+            try
+            {
+                int num = int.Parse(str);
+                Console.WriteLine(num);
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Bad String");
+            }
+        }
+    }
+    
+}
