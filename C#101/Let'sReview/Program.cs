1e29d57382b17c66547004a1618bb7c8712ada7a
Erhan Özgüç <eozguc@hotmail.com>
2022-04-25 23:23:47 +0300

    Let's Review
    Day 6: Let's Review

---
+++ C#101/Let'sReview/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Let_sReview
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var N = int.Parse(Console.ReadLine());
+
+            for (var i = 0; i < N; i++)
+            {
+                var str = Console.ReadLine();
+
+                for (var j = 0; j < str.Length; j++)
+                {
+                    if (j % 2 == 0) Console.Write(str[j]);
+                }
+
+                Console.Write(" ");
+
+                for (var j = 0; j < str.Length; j++)
+                {
+                    if (j % 2 != 0) Console.Write(str[j]);
+                }
+
+                Console.Write(Environment.NewLine);
+            }
+        }
+    }
+}
