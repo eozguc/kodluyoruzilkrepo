c0d404743c688aa2df00572ec2ecc4641b2857c5
Erhan Özgüç <eozguc@hotmail.com>
2022-04-26 02:07:41 +0300

    Dictionaries and Maps
    Day 8: Dictionaries and Maps

---
+++ C#101/DictionariesAndMaps/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DictionariesAndMaps
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+			int n = Int32.Parse(Console.ReadLine());
+			Dictionary<string,
+			string> phonebook = new Dictionary<string,
+			string>();
+
+			for (int i = 0; i < n; i++)
+			{
+				string[] line = Console.ReadLine().Split(' ');
+				phonebook[line[0]] = line[1];
+			}
+
+			string name;
+			while ((name = Console.ReadLine()) != null)
+			{
+				if (phonebook.ContainsKey(name))
+				{
+					Console.WriteLine(name + "=" + phonebook[name]);
+				}
+				else
+				{
+					Console.WriteLine("Not found");
+				}
+			}
+		}
+    }
+}
