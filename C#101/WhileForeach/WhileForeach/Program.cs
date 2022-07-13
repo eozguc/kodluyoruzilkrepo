afee265216d51d4195dc76e876d272a5d0227e77
Erhan Özgüç <eozguc@hotmail.com>
2022-07-13 23:10:01 +0300

    while döngüsü
    *****while döngü deyimi*****

---
+++ C#101/WhileForeach/WhileForeach/Program.cs
@@ -0,0 +1,43 @@
+﻿
+using System;
+
+namespace WhileForeach;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // *****while döngü deyimi*****
+
+        // // 1 den başlayarak konsoldan girilen sayıya kadar (sayı dahil) ortalama hesaplayıp konsala yazdırıldı.
+        // Console.Write("Lütfen bir sayı giriniz: ");
+        // int sayi = int.Parse(Console.ReadLine());
+        // int sayac = 1;
+        // int toplam = 0;
+        // while (sayac <= sayi)
+        // {
+        //     toplam += sayac;
+        //     sayac++;
+        // }
+        // Console.WriteLine(toplam / sayi);            
+
+
+        // // 'a' dan 'z' ye kadar tüm harfler konsola yazdırıldı.            
+        // char character = 'a';
+        // while (character < 'z')
+        // {
+        //     Console.Write(character);
+        //     character++;
+        // }
+
+
+        // *****Foreach döngü deyimi*****
+
+        string[] arabalar = { "BMW", "Ford", "Toyota", "Nissan" };
+        foreach (var araba in arabalar)
+        {
+            Console.WriteLine(araba);
+        }
+
+    }
+}
