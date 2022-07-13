39bc488e8df1eade684296b15568655289aeca27
Erhan Özgüç <eozguc@hotmail.com>
2022-07-13 23:29:48 +0300

    MethodOverloading
    MethodOverloading

---
+++ C#101/MethodOverloading/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+namespace MethodOverloading
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //out parametreler
+            string sayi = "2432";
+
+            //sayıyı çevirip çevirmediğine bak ve çevirebiliyorsan sonucunda bu sayı şu şekilde geri dönsün
+            bool sonuc = int.TryParse(sayi, out int outSayi);
+            if (sonuc)
+            {
+                Console.WriteLine("Başarılı");
+                Console.WriteLine(outSayi);
+            }
+            else
+            {
+                Console.WriteLine("Başarısız");
+            }
+
+            Metodlar metodlar = new Metodlar();
+            metodlar.Topla(8,3,out int toplamSonuc);
+            Console.WriteLine(toplamSonuc);
+
+            //method overloading 
+            int i = 213;
+            metodlar.EkranaYazdir(i);
+
+            metodlar.EkranaYazdir("C","#");
+            //metod imzası
+            //metodAdı + parametre sayisi + parametre
+            
+
+        }
+    }
+
+    class Metodlar
+    {
+        public void Topla(int a,int b,out int toplam)
+        {
+            toplam = a+b;
+        }
+
+        public void EkranaYazdir(string veri)
+        {
+            Console.WriteLine(veri);
+        }
+        public void EkranaYazdir(int veri)
+        {
+            Console.WriteLine(veri);
+        }
+        public void EkranaYazdir(string veri1,string veri2)
+        {
+            Console.WriteLine(veri1+veri2);
+        }
+    }
+}
